561bdeef32983f71b386675876653b287bd5fc2d
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2015-07-09 11:23:43 +0530

    minor changes

--- feedback-front/static/js/charts.js
+++ feedback-front/static/js/charts.js
@@ -1,3 +1,4 @@
+// function renderBarChart will render the bar chart
 function renderBarChart(data, chartDiv){
   nv.addGraph(function() {
     var chart = nv.models.discreteBarChart()
@@ -16,29 +17,30 @@ function renderBarChart(data, chartDiv){
     nv.utils.windowResize(chart.update);
 
     return chart;
-    });
-  }
+  });
+}
           
+// function renderPieChart will render Pie Chart          
 function renderPieChart(data, chartDiv){
-  //Donut chart example
-nv.addGraph(function() {
-  var chart = nv.models.pieChart()
-      .x(function(d) { return d.label })
-      .y(function(d) { return d.value })
-      .showLabels(true)     //Display pie labels
-      .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
-      .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
-      .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
-      .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
-      ;
+  nv.addGraph(function() {
+    var chart = nv.models.pieChart()
+                  .x(function(d) { return d.label })
+                  .y(function(d) { return d.value })
+                  .showLabels(true)     //Display pie labels
+                  .labelThreshold(.05)  //Configure the minimum slice size for labels to show up
+                  .labelType("percent") //Configure what type of data to show in the label. Can be "key", "value" or "percent"
+                  .donut(true)          //Turn on Donut mode. Makes pie chart look tasty!
+                  .donutRatio(0.35)     //Configure how big you want the donut hole size to be.
+                  ;
 
-    d3.select(chartDiv+ ' svg')
+      d3.select(chartDiv+ ' svg')
         .datum(data)
         .transition().duration(350)
-        .call(chart);
+        .call(chart)
+        ;
 
-  return chart;
-});
+    return chart;
+  });
 }
 
 
