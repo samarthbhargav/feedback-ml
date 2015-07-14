4fbdef5653a6f7b25b4edf456db232c535e44d66
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2015-07-14 14:04:33 +0530

    included add_dataset.html

--- feedback-front/client.py
+++ feedback-front/client.py
@@ -4,8 +4,9 @@ import requests
 
 STATS_RESOURCE = "http://localhost:9999/stats/datasets"
 RECORDS_STATS_RESOURCE = "http://localhost:9999/stats/datasets/{}"
-POST_RECORDS_RESOURCE = "http://localhost:9999/record/{}/"
-RECORDS_PAGE_RESOURCE = "http://localhost:9999/page/{}/{}/{}"
+POST_RECORDS_RESOURCE = "http://localhost:9999/dataset/{}/record/"
+RECORDS_PAGE_RESOURCE = "http://localhost:9999/dataset/{}/records/{}/{}"
+POST_DATASET_RESOURCE = "http://localhost:9999/dataset/"
 
 JSON_HEADERS = {'Content-Type': 'application/json'}
 
@@ -39,6 +40,18 @@ class FeedBackClient(object):
             return True
         else:
             # TODO add more desc message
+            return False
+
+    def save_dataset(self, new_dataset):
+
+        endpoint = POST_DATASET_RESOURCE.format(new_dataset)
+
+        resp = requests.post(endpoint)
+        print resp
+
+        if resp.status_code == 200:
+            return True
+        else:
             return False
 
     def get_records(self, dataset, skip, limit):
--- feedback-front/main.py
+++ feedback-front/main.py
@@ -47,6 +47,10 @@ def record_stats_page(dataset):
 def add_record_page():
     return render_template("add_record.html")
 
+@app.route("/add_dataset")
+def add_dataset_page():
+    return render_template("add_dataset.html")
+
 @app.route("/post_record", methods = ["POST"])
 def post_record():
     try:
@@ -75,6 +79,33 @@ def post_record():
 
     except ValueError, e:
         return render_template("add_record.html", error=e.message)
+
+
+# code for adding new record
+@app.route("/post_dataset", methods=["POST"])
+def post_dataset():
+    try:
+        data = request.form
+        print data
+        keys = ["DatasetName"]
+
+
+    #   if key in keys:
+    #       if key is None or len(key) == 0:
+    #              raise ValueError("{} cannot be empty".format(key))
+        
+        new_dataset = {
+            "name" : data["DatasetName"]
+        }
+
+        if client.save_dataset(new_dataset):
+            return render_template("add_dataset.html", success="Dataset added successfully")
+        else:
+            return render_template("add_dataset.html", error="Some error occurred while trying to create new dataset")
+
+    except ValueError, e:
+            return render_template("add_dataset.html", error=e.message)
+
 
 @app.route("/records/<string:dataset>/<int:skip>/<int:limit>")
 def records(dataset, skip, limit):
